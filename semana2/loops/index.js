305ec73407c9f50dd382322d2b69470028f5e490
brunopogianmallta <brunomalltaads@gmail.com>
2021-05-12 16:54:57 -0300

    Exercicios loops-desafio não concluido

---
+++ semana2/loops/index.js
@@ -0,0 +1,76 @@
+                                //Exercícios de interpretação de código
+
+
+ /*
+    1-
+        a)-O código está contando de 0 a 5, o resultado impresso será, 0,1,2,3,4.
+    --------------------------------------------------------------------------------
+    2-
+       a)- vai ser impresso os números 19,21,23,25,27,30
+       b)-
+
+    3-
+     a)-0,00,000,000
+*/
+
+
+/*exercicio de escrita*/
+
+/*1
+
+     const temPets = Number(prompt("Quantos bichinhos de estimação você tem?"))
+     let arrayNomes =[]
+    
+     if(temPets === 0){
+         console.log("Que pena!Você pode adotar um Pet!")
+     } else if(temPets > 0){
+         for(let i = 0; temPets > i;i++){
+            let solicitaNome = prompt("Que legal!Qual o nome deles?")
+            arrayNomes.push(solicitaNome)
+         }
+         
+     }
+     console.log(arrayNomes)
+*/
+
+
+        let arrayOriginal = [80,30,130, 40, 60,21, 70, 120,90,103, 110, 55,]
+        
+     /*2
+
+     
+
+
+     function imprimeValorOriginal(){
+         for(let i = 0; i < arrayOriginal.length; i++){
+            console.log(arrayOriginal[i])
+         }
+     }
+
+     function valorDivididoPordez (){
+         for(let i = 0; i < arrayOriginal.length;i++){
+            console.log(arrayOriginal[i]/10)
+         }
+         
+     }
+     */
+
+     //c) Escreva um programa que **crie** um novo array contendo, 
+     //somente, os números pares do array original e **imprima** esse novo array
+/*
+     function contemParesApenas(){
+         for(let i = 0 ; i < arrayOriginal.length; i++){
+              if(arrayOriginal[i] %2 === 0){
+                  console.log(arrayOriginal[i])
+              }
+             }
+         }
+
+*/
+     
+
+
+
+       //contemParesApenas()
+     //valorDivididoPordez()
+     //imprimeValorOriginal()
