7017145d9e1470c2d1093058bcdccd52dbddba89
brunopogianmallta <brunomalltaads@gmail.com>
2021-05-13 16:47:28 -0300

    exercicios funcoes-de-array

---
+++ semana2/funcoes-de-array/index.js
@@ -0,0 +1,99 @@
+/*                                  
+                                    EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO
+ 
+ 1-
+        a)-  vai sair na tela o nome dos usarios.
+------------------------------------------------------------------------------------------------------
+2-vai logar  nome: {"Amanda Rangel", },{ nome: "Laís Petra" },{ nome: "Letícia Chijo"}
+
+-------------------------------------------------------------------------------------------------------
+
+3-
+    a)- "Mandi" ,"laura"
+  
+-------------------------------------------------------------------------------------------------------
+                                     EXERCICIOS DE ESCRITA DE CÓDIGO
+   1-                                  
+*/
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+/*
+ //a-TCHÂNANNNNNN!
+    const nomeDosDoguinhos = pets.map((dog) =>{
+        return dog.nome
+    })
+
+    console.log(nomeDosDoguinhos)
+
+  
+//b-
+const apenasSalsicha = pets.filter ((dogRaca) =>{
+    return  dogRaca.raca ==="Salsicha"
+})
+
+console.log (apenasSalsicha)
+ 
+//c-
+
+    const checaRaca = pets.filter((racaDoDesconto) =>{
+        return racaDoDesconto === 'Poodle'
+    })
+
+    const descontoPoodle = pets.map((racaDoDesconto )=>{
+        return  racaDoDesconto.nome
+
+    })
+
+   
+    console.log("Você ganhou um cupom de desconto de 10% para tosar o/a",descontoPoodle )
+ */
+
+ const produtos = [
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]  
+
+    /*a)-
+    const apenasNome = apenasBebida.map((nomeProduto) =>{
+        return nomeProduto.nome
+    })
+    
+    console.log(apenasNome)
+*/
+
+    /*b)-
+
+    const nomeEpreco = faiô não entendi
+    */
+
+    //c-
+
+ 
+
+/*
+        const apenasBebida = produtos.filter((nomeProduto) =>{
+            return nomeProduto.categoria === "Bebidas"
+        })
+
+        console.log(apenasBebida)
+
+ */
+
+        //d-
+
+
+        //Gente não consegui fazer vai acabar o tempo.
