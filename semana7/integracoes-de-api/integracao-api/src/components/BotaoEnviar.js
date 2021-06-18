8f00ced64fbc1a38d806a9afd47fda85ba8c2691
brunopogianmallta <brunomalltaads@gmail.com>
2021-06-18 17:09:34 -0600

    base do projeto labfy

---
+++ semana7/integracoes-de-api/integracao-api/src/components/BotaoEnviar.js
@@ -0,0 +1,13 @@
+//importando o módulo React
+import React from 'react';
+
+//craindo o componente
+class BotaoEnviar extends React.Component{
+    render(){
+        return <button>Enviar</button>
+    }
+}
+
+
+//exportando
+export default BotaoEnviar; 
